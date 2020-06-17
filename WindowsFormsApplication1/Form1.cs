b026c402cbef27eddb8f6b6b3fc93fc0a9a8ee74
Yağızcan Yavuz <yagizcanyavuz@outlook.com>
2020-06-18 00:30:57 +0300

    Add project files.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, toplam = 0;
+            for (i = 0; i <= 100; i+=2)
+            {
+                toplam = toplam + i;
+                listBox1.Items.Add(i);
+                label2.Text = toplam.ToString();
+            }
+        }
+    }
+}
